7765a71d9230d1ba05ddf79dcb18b88d3bd428ea
Nehuen Olivera <nehuenolivera@gmail.com>
2025-12-10 20:56:09 +1000

    Update supabase types

--- types/supabase.ts
+++ types/supabase.ts
@@ -4,343 +4,359 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
   // Allows to automatically instantiate createClient with right options
   // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
   __InternalSupabase: {
-    PostgrestVersion: "13.0.5";
-  };
+    PostgrestVersion: "13.0.5"
+  }
   graphql_public: {
     Tables: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
       graphql: {
         Args: {
-          extensions?: Json;
-          operationName?: string;
-          query?: string;
-          variables?: Json;
-        };
-        Returns: Json;
-      };
-    };
+          extensions?: Json
+          operationName?: string
+          query?: string
+          variables?: Json
+        }
+        Returns: Json
+      }
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
   public: {
     Tables: {
       attendees: {
         Row: {
-          created_at: string;
-          event_id: string;
-          status: string;
-          updated_at: string;
-          user_id: string;
-        };
+          created_at: string
+          event_id: string
+          status: string
+          updated_at: string
+          user_id: string
+        }
         Insert: {
-          created_at?: string;
-          event_id: string;
-          status?: string;
-          updated_at?: string;
-          user_id: string;
-        };
+          created_at?: string
+          event_id: string
+          status?: string
+          updated_at?: string
+          user_id: string
+        }
         Update: {
-          created_at?: string;
-          event_id?: string;
-          status?: string;
-          updated_at?: string;
-          user_id?: string;
-        };
+          created_at?: string
+          event_id?: string
+          status?: string
+          updated_at?: string
+          user_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "attendees_event_id_fkey";
-            columns: ["event_id"];
-            isOneToOne: false;
-            referencedRelation: "events";
-            referencedColumns: ["id"];
+            foreignKeyName: "attendees_event_id_fkey"
+            columns: ["event_id"]
+            isOneToOne: false
+            referencedRelation: "events"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "attendees_user_id_fkey";
-            columns: ["user_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
-          }
-        ];
-      };
+            foreignKeyName: "attendees_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       events: {
         Row: {
-          created_at: string | null;
-          description: string | null;
-          duration_minutes: number;
-          id: string;
-          is_cancelled: boolean | null;
-          location: string | null;
-          max_participants: number;
-          name: string;
-          post_schedule_message: string | null;
-          start_at: string;
-          suggested_price: number | null;
-          updated_at: string | null;
-        };
+          created_at: string | null
+          description: string | null
+          duration_minutes: number
+          id: string
+          is_cancelled: boolean | null
+          location: string | null
+          max_participants: number
+          name: string
+          post_schedule_message: string | null
+          start_at: string
+          suggested_price: number | null
+          updated_at: string | null
+        }
         Insert: {
-          created_at?: string | null;
-          description?: string | null;
-          duration_minutes?: number;
-          id?: string;
-          is_cancelled?: boolean | null;
-          location?: string | null;
-          max_participants?: number;
-          name: string;
-          post_schedule_message?: string | null;
-          start_at: string;
-          suggested_price?: number | null;
-          updated_at?: string | null;
-        };
+          created_at?: string | null
+          description?: string | null
+          duration_minutes?: number
+          id?: string
+          is_cancelled?: boolean | null
+          location?: string | null
+          max_participants?: number
+          name: string
+          post_schedule_message?: string | null
+          start_at: string
+          suggested_price?: number | null
+          updated_at?: string | null
+        }
         Update: {
-          created_at?: string | null;
-          description?: string | null;
-          duration_minutes?: number;
-          id?: string;
-          is_cancelled?: boolean | null;
-          location?: string | null;
-          max_participants?: number;
-          name?: string;
-          post_schedule_message?: string | null;
-          start_at?: string;
-          suggested_price?: number | null;
-          updated_at?: string | null;
-        };
-        Relationships: [];
-      };
+          created_at?: string | null
+          description?: string | null
+          duration_minutes?: number
+          id?: string
+          is_cancelled?: boolean | null
+          location?: string | null
+          max_participants?: number
+          name?: string
+          post_schedule_message?: string | null
+          start_at?: string
+          suggested_price?: number | null
+          updated_at?: string | null
+        }
+        Relationships: []
+      }
       users: {
         Row: {
-          created_at: string | null;
-          email: string;
-          emergency_contact_name: string | null;
-          emergency_contact_phone: string | null;
-          emergency_contact_relationship: string | null;
-          first_name: string;
-          id: string;
-          last_name: string;
-          phone: string | null;
-          role: string;
-          waiver_signed: boolean;
-        };
+          created_at: string | null
+          email: string
+          emergency_contact_name: string | null
+          emergency_contact_phone: string | null
+          emergency_contact_relationship: string | null
+          first_name: string
+          id: string
+          last_name: string
+          phone: string | null
+          role: string
+          waiver_signed: boolean
+        }
         Insert: {
-          created_at?: string | null;
-          email: string;
-          emergency_contact_name?: string | null;
-          emergency_contact_phone?: string | null;
-          emergency_contact_relationship?: string | null;
-          first_name: string;
-          id: string;
-          last_name: string;
-          phone?: string | null;
-          role?: string;
-          waiver_signed?: boolean;
-        };
+          created_at?: string | null
+          email: string
+          emergency_contact_name?: string | null
+          emergency_contact_phone?: string | null
+          emergency_contact_relationship?: string | null
+          first_name: string
+          id: string
+          last_name: string
+          phone?: string | null
+          role?: string
+          waiver_signed?: boolean
+        }
         Update: {
-          created_at?: string | null;
-          email?: string;
-          emergency_contact_name?: string | null;
-          emergency_contact_phone?: string | null;
-          emergency_contact_relationship?: string | null;
-          first_name?: string;
-          id?: string;
-          last_name?: string;
-          phone?: string | null;
-          role?: string;
-          waiver_signed?: boolean;
-        };
-        Relationships: [];
-      };
-    };
+          created_at?: string | null
+          email?: string
+          emergency_contact_name?: string | null
+          emergency_contact_phone?: string | null
+          emergency_contact_relationship?: string | null
+          first_name?: string
+          id?: string
+          last_name?: string
+          phone?: string | null
+          role?: string
+          waiver_signed?: boolean
+        }
+        Relationships: []
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      admin_get_events: { Args: never; Returns: Json };
+      admin_get_events: { Args: never; Returns: Json }
       book_attendee: {
-        Args: { p_event_id: string };
+        Args: { p_event_id: string }
         Returns: {
-          r_event_id: string;
-          r_status: string;
-          r_user_id: string;
-        }[];
-      };
+          r_event_id: string
+          r_status: string
+          r_user_id: string
+        }[]
+      }
+      cancel_booking: { Args: { eid: string }; Returns: boolean }
+      get_all_users_for_admin: {
+        Args: never
+        Returns: {
+          email: string
+          emergency_contact_name: string
+          emergency_contact_phone: string
+          emergency_contact_relationship: string
+          first_name: string
+          id: string
+          last_name: string
+          phone: string
+          waiver_signed: boolean
+        }[]
+      }
       get_events_with_info: {
-        Args: { p_user_id?: string };
+        Args: { p_user_id?: string }
         Returns: {
-          booked_count: number;
-          description: string;
-          duration_minutes: number;
-          id: string;
-          location: string;
-          max_participants: number;
-          name: string;
-          post_schedule_message: string;
-          spots_left: number;
-          start_at: string;
-          suggested_price: number;
-          user_joined: boolean;
-        }[];
-      };
+          booked_count: number
+          description: string
+          duration_minutes: number
+          id: string
+          location: string
+          max_participants: number
+          name: string
+          post_schedule_message: string
+          spots_left: number
+          start_at: string
+          suggested_price: number
+          user_joined: boolean
+        }[]
+      }
       get_events_with_spots: {
-        Args: { p_user_id: string };
+        Args: { p_user_id: string }
         Returns: {
-          description: string;
-          duration_minutes: number;
-          id: string;
-          is_cancelled: boolean;
-          max_participants: number;
-          name: string;
-          post_schedule_message: string;
-          spots_left: number;
-          start_at: string;
-          suggested_price: number;
-          user_status: string;
-        }[];
-      };
-    };
+          description: string
+          duration_minutes: number
+          id: string
+          is_cancelled: boolean
+          max_participants: number
+          name: string
+          post_schedule_message: string
+          spots_left: number
+          start_at: string
+          suggested_price: number
+          user_status: string
+        }[]
+      }
+      is_booking_cancelled: {
+        Args: { p_event_id: string; p_user_id: string }
+        Returns: boolean
+      }
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">;
+type DatabaseWithoutInternals = Omit<Database, "__InternalSupabase">
 
-type DefaultSchema = DatabaseWithoutInternals[Extract<
-  keyof Database,
-  "public"
->];
+type DefaultSchema = DatabaseWithoutInternals[Extract<keyof Database, "public">]
 
 export type Tables<
   DefaultSchemaTableNameOrOptions extends
     | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
         DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
-    : never = never
+    : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? (DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
       DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
   : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
-      DefaultSchema["Views"])
-  ? (DefaultSchema["Tables"] &
-      DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
-      Row: infer R;
-    }
-    ? R
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
     : never
-  : never;
 
 export type TablesInsert<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-  ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-      Insert: infer I;
-    }
-    ? I
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
     : never
-  : never;
 
 export type TablesUpdate<
   DefaultSchemaTableNameOrOptions extends
     | keyof DefaultSchema["Tables"]
     | { schema: keyof DatabaseWithoutInternals },
   TableName extends DefaultSchemaTableNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
-    : never = never
+    : never = never,
 > = DefaultSchemaTableNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
-  ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
-      Update: infer U;
-    }
-    ? U
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
     : never
-  : never;
 
 export type Enums<
   DefaultSchemaEnumNameOrOptions extends
     | keyof DefaultSchema["Enums"]
     | { schema: keyof DatabaseWithoutInternals },
   EnumName extends DefaultSchemaEnumNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
-    : never = never
+    : never = never,
 > = DefaultSchemaEnumNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
-  ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
-  : never;
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+    : never
 
 export type CompositeTypes<
   PublicCompositeTypeNameOrOptions extends
     | keyof DefaultSchema["CompositeTypes"]
     | { schema: keyof DatabaseWithoutInternals },
   CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
-    schema: keyof DatabaseWithoutInternals;
+    schema: keyof DatabaseWithoutInternals
   }
     ? keyof DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
-    : never = never
+    : never = never,
 > = PublicCompositeTypeNameOrOptions extends {
-  schema: keyof DatabaseWithoutInternals;
+  schema: keyof DatabaseWithoutInternals
 }
   ? DatabaseWithoutInternals[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
   : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
-  ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
-  : never;
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
 
 export const Constants = {
   graphql_public: {
@@ -349,4 +365,4 @@ export const Constants = {
   public: {
     Enums: {},
   },
-} as const;
+} as const
