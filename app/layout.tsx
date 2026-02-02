ff2c6edfe48c4fbc7c4851ab1bf5397fd99dff14
Nehuen Olivera <nehuenolivera@gmail.com>
2026-02-02 14:14:04 +1000

    Change icon in browser

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,16 +22,16 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/Design_Favicon.svg", sizes: "16x16", type: "image/png" },
+      { url: "/Design_Favicon.svg", sizes: "32x32", type: "image/png" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
     other: [
       // Android / PWA icons
-      { rel: "android-chrome-192x192", url: "/android-chrome-192x192.png" },
-      { rel: "android-chrome-512x512", url: "/android-chrome-512x512.png" },
+      { rel: "/Design_Favicon.svg", url: "/Design_Favicon.svg" },
+      { rel: "/Design_Favicon.svg", url: "/Design_Favicon.svg" },
     ],
   },
 
